a9c2ba659906c84f805c418dd2321e26b2a50bfd
nidhi <inidhi99@gmail.com>
2022-12-07 18:59:23 -0500

    added client componenets/signupform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -4,16 +4,18 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 // Import the GraphQL mutation
 import { ADD_USER } from '../../utils/mutations';
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
   // set state for form validation
   const [validated] = useState(false);
+
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
   const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
@@ -36,15 +38,6 @@ const SignupForm = () => {
         variables: { ...userFormData },
       });
       Auth.login(data.addUser.token);
-      // const response = await createUser(userFormData);
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
