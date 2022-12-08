2b3272a83e62d4f4528d402e0b5c67369f24031c
nidhi <inidhi99@gmail.com>
2022-12-07 22:08:36 -0500

    updated client/compenets/signupform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,7 +4,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 // Import the `useMutation()` hook from Apollo Client
 import { useMutation } from '@apollo/client';
 // Import the GraphQL mutation
-import { LOGIN_USER } from '../../src/utils/mutations';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
