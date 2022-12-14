e6d08e26307a9fdc9c82294e88a41ad4d082f384
nidhi <inidhi99@gmail.com>
2022-12-14 14:29:55 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+// DEPENDENCIES
+require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -20,14 +21,12 @@ app.use(express.json());
 
 // if in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
-
-// app.use(routes);
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
